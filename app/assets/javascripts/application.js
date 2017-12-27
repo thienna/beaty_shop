b57968fecd745862eb91412e6f969ee23214f937
Mike <ng.anhthien@gmail.com>
2017-12-27 13:59:48 +0700

    ref Task #152762 - Home page Footer (add libs)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,25 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery2
+//= require jquery_ujs
+//= require bootstrap-sprockets
+//= require chosen.jquery.min
+//= require Modernizr.min
+//= require jquery-ui
+//= require jquery.parallax-1.1.3.min
+//= require jquery.plugin.min
+//= require jquery.countdown.min
+//= require jquery.magnific-popup.min
+//= require wow
+//= require jquery.mCustomScrollbar.concat.min
+//= require jquery.actual.min
+//= require masonry.pkgd.min
+//= require imagesloaded.pkgd.min
+//= require isotope.pkgd.min
+//= require easyzoom.min
+//= require jquery.hoverdir.min
+//= require masonry.min
+//= require functions.min
 //= require rails-ujs
 //= require turbolinks
-//= require_tree .
