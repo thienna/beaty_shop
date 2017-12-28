3a927bfab7782262c4a816a15e9c10492794aaef
Mike <ng.anhthien@gmail.com>
2017-12-28 13:49:05 +0700

    ref Task #151805 Cart page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
     root "home#index"
     get "/product", to: "products#show"
+    get "/cart", to:  "carts#index"
   end
 end
