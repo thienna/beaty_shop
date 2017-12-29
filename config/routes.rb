ae06f668f4b9726eb0b502b3f5544b938530af04
Mike <ng.anhthien@gmail.com>
2017-12-29 12:53:32 +0700

    ref Task #151807 Authentication page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
     root "home#index"
     get "/product", to: "products#show"
+    get "/login", to: "sessions#new"
     resource :cart, only: :show
     resources :orders, only: :index
   end
