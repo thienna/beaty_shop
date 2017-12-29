f2f7ad72c1ed8058f58fcd88ac6a2da4fb39cd04
Mike <ng.anhthien@gmail.com>
2017-12-29 12:53:32 +0700

    ref Task #153200 Design Layout Admin Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
+    namespace :admin do
+      root "dashboard#index"
+    end
+
     root "home#index"
     get "/product", to: "products#show"
     get "/login", to: "sessions#new"
