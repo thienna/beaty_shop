3e6e0c1f12b92574f480fae8d95d78607c6146c8
Mike <ng.anhthien@gmail.com>
2018-01-07 16:15:59 +0700

    ref Task #151821 Category module - TSX

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,31 @@
 class Product < ApplicationRecord
+  ATTRIBUTES = [:name, :description, :quantity, :price,
+    :price_sale, :picture, category_ids: []].freeze
+
   has_many :product_categories, dependent: :destroy
   has_many :categories, through: :product_categories
   has_many :order_details, dependent: :destroy
+
+  mount_uploader :picture, PictureUploader
+
+  validates :name, presence: true,
+    length: {maximum: Settings.product.name.maximum}
+  validates :quantity, presence: true,
+    numericality:
+      {only_integer: true, greater_than: Settings.product.quantity.minimum}
+  validates :price, presence: true, numericality: true
+  validates :price_sale, numericality: true, allow_nil: true
+  validates :picture, presence: true
+  validates :description, presence: true
+  validates :categories, presence: true
+  validate :picture_size
+
+  scope :newest, ->{order created_at: :desc}
+
+  private
+
+  def picture_size
+    return unless picture.size > Settings.product.picture.size.megabytes
+    errors.add :picture, I18n.t("admin_page.products.validates_size_failed")
+  end
 end
