3696abc0b205fd0dcb2c1180859cbf0c56200d64
Mike <ng.anhthien@gmail.com>
2018-01-26 14:00:09 +0700

    edit email template

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,4 +1,6 @@
 class OrderMailer < ApplicationMailer
+  include ApplicationHelper
+
   def order_information order
     @order = order
     mail to: order.customer_email, subject: t("w_order_info")
